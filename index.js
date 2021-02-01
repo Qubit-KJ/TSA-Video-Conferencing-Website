ab212237be9f6fdb4db6c185401c06352a8829c2
Ben Shobama <72891451+Qubit-KJ@users.noreply.github.com>
2021-02-01 16:54:41 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
+
+<script>
+
+function myMap() {
+var mapProp= {
+ center:new google.maps.LatLng(51.508742,-0.120850),
+ zoom:5,
+};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+function myFunction() {
+  var copyText = document.getElementById("myDogeID");
+  copyText.select();
+  copyText.setSelectionRange(0, 32)
+  document.execCommand("copy");
+}
+
+</script>
